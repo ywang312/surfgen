cfb2f9dd9e7ae89a36afe8cc27ae74ecdf7750a5
cthree-40 <clmalbon@gmail.com>
2017-05-17 12:41:24 -0400

    Removed atom-switching functionality from getneighbor. Not necessary.

--- utilities/getneighbor/getneighbor.f90
+++ utilities/getneighbor/getneighbor.f90
@@ -21,10 +21,6 @@ program getneighbor
   integer :: nc, j
   double precision, external :: dnrm2
 
-  character(255) :: answer
-  double precision, dimension(:), allocatable :: tmp_geometry, tmp
-  integer :: a1, a2
-  
   call initpotential()
   call getinfo(num_atoms, num_states)
   allocate(input_geometry(num_atoms*3))
@@ -57,56 +53,6 @@ program getneighbor
   print "(f14.2)", energy*219474.63
   print *, "Neighbor: ", neighbor
   print *, "Distance: ", dist
-
-  ! Atom permutations
-  do while (.true.)
-          print *, "Would you like to permute identical atoms for this geometry?"
-          read *, answer
-
-          if (trim(adjustl(answer)) .eq. "no" .or.    &
-                  trim(adjustl(answer)) .eq. "No" .or.&
-                  trim(adjustl(answer)) .eq. "NO") exit
-
-          if (trim(adjustl(answer)) .eq. "yes" .or.     &
-                  trim(adjustl(answer)) .eq. "Yes" .or. &
-                  trim(adjustl(answer)) .eq. "YES") then
-
-                  if(.not. allocated(tmp)) allocate(tmp(3))
-                  if(.not. allocated(tmp_geometry)) &
-                          allocate(tmp_geometry(3*num_atoms))
-                  
-                  print *, "Atom 1: "
-                  read *, a1
-                  print *, "Atom 2: "
-                  read *, a2
-
-                  tmp_geometry = input_geometry
-                  tmp_geometry(a1*3-2:a1*3) = input_geometry(a2*3-2:a2*3)
-                  tmp_geometry(a2*3-2:a2*3) = input_geometry(a1*3-2:a1*3)
-                  print *, "New geometry: "
-                  print "(3f14.8)", tmp_geometry
-
-                  call EvaluateSurfgen(tmp_geometry, energy, cgrads, hmat, &
-                          dcgrads)
-                  print "(f14.2)", energy*219474.63
-                  call buildWBMat(tmp_geometry, igeom, bmat)
-                  call getmind(igeom, dist, neighbor, eerr)
-                  if (cvanish <= 0) then
-                          nc = ndcoord
-                  else
-                          nc=0
-                          do j=1, ndcoord
-                                  if (dnrm2(3*num_atoms,bmat(dcoordls(j),1),ncoord)>cvanish) &
-                                          nc = nc + 1
-                          end do
-                  end if
-                  dist = dist / sqrt(dble(nc))
-                  print *, "Neighbor: ", neighbor
-                  print *, "Distance: ", dist
-          end if
-  end do
-                  
-  
                   
 contains
   subroutine read_geometry_from_file(gmfl, na, gm)
